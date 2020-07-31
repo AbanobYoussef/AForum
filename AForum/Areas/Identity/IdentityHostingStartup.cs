defa3a8ad1c07cd88f5745ed7e6ba576e1c4b185
AbanobYoussef <abanobyoussef1996@gmail.com>
2020-07-31 16:17:02 +0200

    Add project files.

---
+++ AForum/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AForum.Areas.Identity.Data;
+using AForum.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AForum.Areas.Identity.IdentityHostingStartup))]
+namespace AForum.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AForumContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AForumContextConnection")));
+
+                services.AddDefaultIdentity<AForumUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AForumContext>();
+            });
+        }
+    }
+}
